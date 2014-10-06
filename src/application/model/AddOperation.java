54beda6fe6fae6c10731905cd471e6a8aa735589
Toby Retallick <toby.retallick@gmail.com>
2014-10-06 14:55:24 +0100

    Moved bigDecimal converter to its own class

--- src/application/model/AddOperation.java
+++ src/application/model/AddOperation.java
@@ -1,7 +1,6 @@
 package application.model;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 public class AddOperation implements Operation {
@@ -10,20 +9,12 @@ public class AddOperation implements Operation {
 
     @Override
     public String execute(List<String> numbersToAdd) {
-        return add(convertStringToBigDecimal(numbersToAdd));
+        return add(ConvertStringToBigDecimal.convert(numbersToAdd));
     }
 
     private String add(List<BigDecimal> bigDecimals) {
         BigDecimal result = bigDecimals.stream().reduce(BigDecimal.ZERO, BigDecimal::add);
         result.setScale(BigDecimal.ROUND_HALF_UP);
         return total += result.toString();
-    }
-
-    private List<BigDecimal> convertStringToBigDecimal(List<String> numbersToAdd) {
-        List<BigDecimal> bigDecimalList = new ArrayList<>();
-        for(String number : numbersToAdd) {
-            bigDecimalList.add(new BigDecimal(number));
-        }
-        return bigDecimalList;
     }
 }
---
+++ src/application/model/convertStringToBigDecimal.java
@@ -0,0 +1,16 @@
+package application.model;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+class ConvertStringToBigDecimal {
+
+    public static List<BigDecimal> convert(List<String> numbersToOperateOn) {
+        List<BigDecimal> bigDecimalList = new ArrayList<>();
+        for(String number : numbersToOperateOn) {
+            bigDecimalList.add(new BigDecimal(number));
+        }
+        return bigDecimalList;
+    }
+}
